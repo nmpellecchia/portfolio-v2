2c08f18cddccd0c55145e841db5139de047e09d2
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2022-01-14 17:50:48 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <header className="h-screen w-full bg-black text-white p-6 flex flex-col justify-center items-center border-b-8 border-red-600">
-      <h1 className="text-6xl tracking-tighter font-black text-red-600 uppercase">
-        nicolas pellecchia
+    <header className="h-screen w-full bg-black text-red-600 font-black p-6 flex flex-col justify-center items-center border-b-8 border-red-600">
+      <h1 className="text-7xl tracking-tighter uppercase text-center">
+        nicolas
       </h1>
-      <span className="text-6xl text-red-600 tracking-wider font-black">
-        - • -
-      </span>
-      <h2 className="font-black uppercase mt-5">desarrollador front-end</h2>
+      <h1 className="text-6xl tracking-tighter uppercase text-center">
+        pellecchia
+      </h1>
+      <span className="text-6xl tracking-wider">- • -</span>
+      <h2 className="text-white uppercase mt-5">desarrollador front-end</h2>
     </header>
   );
 }
