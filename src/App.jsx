f9c0f88db96b1117c76c7d5e3ef3285b6c21a22c
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2022-01-18 23:18:21 -0300

    Added the bg image and styled header

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/Home';
+import AboutMe from './components/About';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
+    <div className="w-full bg-blackest-color h-full text-whitest-color text-lg font-main bg-[url('./src/imgs/bg-img.jpg')] bg-cover">
       <HomePage />
+      <AboutMe />
+      <Footer />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <header className="h-screen w-full bg-black text-red-600 font-black p-6 flex flex-col justify-center items-center border-b-8 border-red-600">
-      <h1 className="text-7xl tracking-tighter uppercase text-center">
-        nicolas
-      </h1>
-      <h1 className="text-6xl tracking-tighter uppercase text-center">
-        pellecchia
-      </h1>
-      <span className="text-6xl tracking-wider">- • -</span>
+    <header className="h-screen w-full bg-blackest-color bg-opacity-50 text-main-color text-transparent p-6 font-black flex flex-col justify-center items-center border-b-8 border-accent-color">
+      <div className="text-8xl tracking-tighter text-center font-header bg-gradient-to-r from-main-color to-accent-color bg-clip-text">
+        <h1 className="px-4">Nicolas</h1>
+        <h1 className="px-4">Pellecchia</h1>
+      </div>
+      <span className="text-6xl tracking-wider bg-gradient-to-br from-main-color to-accent-color bg-clip-text">
+        ⚊ • ⚊
+      </span>
       <h2 className="text-white uppercase mt-5">desarrollador front-end</h2>
     </header>
   );
