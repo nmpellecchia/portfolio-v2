1758d207738641197c8d890c6bfbe7faedf5d8e3
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2022-01-20 00:32:38 -0300

    project changes
    - Now projects have the URLs to the repos and gh-pages. Allowing users to see them live.
    - Changed projectsList component to have those URLs and other stylistic changes
    -Added the logo on the site for easier navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/Home';
 import AboutMe from './components/About';
 import Footer from './components/Footer';
 import ProjectList from './components/ProjectList';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <div className="w-full bg-blackest-color h-full text-whitest-color text-lg font-main bg-[url('./src/imgs/bg-img.jpg')] bg-cover">
+      <Logo />
       <HomePage />
       <ProjectList />
       <AboutMe />
--- src/resources/myProjects.js
+++ src/resources/myProjects.js
@@ -3,23 +3,35 @@ import countriesPageImg from '../imgs/projects/countries_2.png';
 import triviaPageImg from '../imgs/projects/trivia.png';
 
 class Project {
-  constructor(img, name, definition) {
+  constructor(img, name, definition, repoURL, pageURL) {
     this.img = img;
     this.name = name;
     this.definition = definition;
+    this.repoURL = repoURL;
+    this.pageURL = pageURL;
   }
 }
 
 const project1 = new Project(
   mockupPageImg,
-  'Tree-umph company mockup page',
-  'def'
+  'Tree-umph company',
+  'Simulacro de página web de una compañía',
+  'https://github.com/nmpellecchia/product-website-mockup',
+  'https://nmpellecchia.github.io/product-website-mockup/'
 );
-const project2 = new Project(countriesPageImg, 'Every country list', 'def');
+const project2 = new Project(
+  countriesPageImg,
+  'Listado de países',
+  'Aplicación interactiva para aprender sobre cualquier país del mundo',
+  'https://github.com/nmpellecchia/countries-api',
+  'https://nmpellecchia.github.io/countries-api/'
+);
 const project3 = new Project(
   triviaPageImg,
-  'General Trivia with retro look',
-  'def'
+  'Trivia con aspecto retro',
+  'Divertido juego de preguntas de conocimiento gral. con un curioso estilo retro',
+  'https://github.com/nmpellecchia/Retro-looking-Trivia',
+  'https://nmpellecchia.github.io/Retro-looking-Trivia/'
 );
 
 export const ProjectBundle = [project1, project2, project3];
